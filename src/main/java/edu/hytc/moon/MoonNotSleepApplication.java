6f20b9f7f718f4f48b25730655c1f40dd48a4094
gucanlin <294403373@qq.com>
2021-04-25 00:31:39 +0900

    add

--- src/main/java/edu/hytc/moon/MoonNotSleepApplication.java
+++ src/main/java/edu/hytc/moon/MoonNotSleepApplication.java
@@ -1,13 +1,41 @@
 package edu.hytc.moon;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MoonNotSleepApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MoonNotSleepApplication.class, args);
+    }
+
+
+    @Bean
+    public TomcatServletWebServerFactory webServerFactory() {
+
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+
+        factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+
+            @Override
+
+            public void customize(Connector connector) {
+
+                connector.setProperty("relaxedPathChars", "\"<>[\\]^`{|}");
+
+                connector.setProperty("relaxedQueryChars", "\"<>[\\]^`{|}");
+
+            }
+
+        });
+
+        return factory;
+
     }
 
 }
